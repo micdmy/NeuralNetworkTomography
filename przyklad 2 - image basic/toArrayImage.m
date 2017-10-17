6cf4fc88fabe5d6fda3fb00bf9187d815e110e38
Michał Dmytruszyński <michal.dmytruszynski@gmail.com>
2017-10-17 15:18:19 +0200

    przyklad 2: mirror image on y axis.

--- przyklad 2 - image basic/toArrayImage.m
+++ przyklad 2 - image basic/toArrayImage.m
@@ -1,10 +1,12 @@
 function [ RGBArray ] = toArrayImage( img )
 %Converts m x n matrix of [0:1] values to 3-D array of true colors
 %Input and output is in grayscale;
+
 [xSize, ySize] = size(img);
+img = img([ySize:-1:1],:); % y-axis mirror, becouse image is displayed top to bottom
 RGBArray = zeros(xSize,ySize,3);
-RGBArray(:,:,1) = img;
-RGBArray(:,:,2) = img;
-RGBArray(:,:,3) = img;
+RGBArray(:,:,1) = img; % set red channel
+RGBArray(:,:,2) = img; % set green channel
+RGBArray(:,:,3) = img; % set blue channel
 end
 
