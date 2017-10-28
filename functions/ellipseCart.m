e9775650cdf72524213e0b7b3544ef369886fe7d
Michał Dmytruszyński <michal.dmytruszynski@gmail.com>
2017-10-28 22:57:14 +0200

    EllipseCart ca return many ellipses.

--- functions/ellipseCart.m
+++ functions/ellipseCart.m
@@ -1,19 +1,36 @@
 function [x,y] = ellipseCart(xpos, ypos, a, b, alfa)
-%Returns ellipse's cartesian coordinates.
+%Returns ellipse's cartesian coordinates. Each row in parameters and output
+%corresponds to one ellipse. Parameters are column vectors.
 %x and y length is 4*max([a,b])
 
-%Parameters:
+%Parameters (column vectors):
 %x,y - center of ellipse
 %a - half of width
 %b - half of height
 %alfa - rotation of ellipse in rads
 
-longerSize = 4 * max([a,b]); %number of counted pixels should be at least 2 * longer ellipse axis 
+xposSize = size(xpos);
+ellipses = xposSize(1);
 
-fi = linspace(0, 2*pi, longerSize); %initial fi vector
-ro = (a .* b) ./ sqrt( (b.*cos(fi)).^2 + (a.*sin(fi)).^2 ); % ellipse polar coordinates equation 
-fi = fi + alfa.*ones(size(fi)); % rotate ellipsy by alfa
+
+points = 4 * max([max(a),max(b)]); %number of counted pixels should be at least 2 * longer ellipse axis 
+%each ellipse contains of 'points' points
+
+firow = linspace(0, 2*pi, points); %row of fi angles 
+licznik = (a .* b) * ones(1, points); % ellipse polar coordinates equation
+mianownik = sqrt( (b*cos(firow)).^2 + (a*sin(firow)).^2 ); % ellipse polar coordinates equation
+ro = licznik ./ mianownik; % ellipse polar coordinates equation
+%now ro contains ro coordinates of ellipses with center in (0,0), each row
+%of ro corresponds to one ellipse
+fi = ones(ellipses, 1) * firow; 
+rotationAngles = alfa * ones(1,points);
+fi = fi + rotationAngles; % rotate ellipses by alfa
+%now fi contains fi coordinates of ellipses rotated according to alfa vector, 
+%with center in (0,0), each row of fi corresponds to one ellipse
 [x, y] = pol2cart(fi, ro); % convert to cartesian
-x = x + xpos .* ones(size(x)); % shift ellipse horizontally
-y = y + ypos .* ones(size(y)); % shift ellipse vertically
+%now each x row contains x coordinates of one ellipse, the same with y
+xshift = xpos * ones(1,points);
+yshift = ypos * ones(1,points);
+x = x + xshift;% shift ellipse horizontally to center specified in xpos
+y = y + xshift;% shift ellipse vertically to center specified in ypos
 end
--- templates/Template7ElipseCoordinatesGeneration.m
+++ templates/Template7ElipseCoordinatesGeneration.m
@@ -1,16 +1,18 @@
 clear all;
 clc;
 
-x0 = 20; %center of ellipse, x coordinate
-y0 = -50; %center of ellipse, y coordinate
-a = 40; %half of horizontal axis
-b = 60; %half of vertical axis
-rotation = pi/6; % rotation of ellipse in rads
+x0 = [5;0]; %center of ellipse, x coordinate
+y0 = [6;0]; %center of ellipse, y coordinate
+a = [3;6]; %half of horizontal axis
+b = [5;3]; %half of vertical axis
+rotation = [0;pi/6]; % rotation of ellipse in rads
 
 [x,y] = ellipseCart(x0, y0, a, b, rotation);
 
 figure(1);
 daspect([1,1,1]);
 hold on;
-plot(x,y);
+plot(x(1,:),y(1,:));
 grid on;
+hold on;
+plot(x(2,:),y(2,:));
